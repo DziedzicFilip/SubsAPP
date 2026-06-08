3b2a607e6f6f93c6b82ada3d44ad03b23a152326
DziedzicFilip <dziedzicfilip00@gmail.com>
2026-06-08 18:23:14 +0200

    refactor(frontend): modularize weather forecast data fetching
    - Created WeatherForecastService.ts to handle API HTTP requests.
    - Implemented useWeatherForecastHook.tsx to manage state and side effects.
    - Added WeatherForecastModel.ts to define TypeScript interfaces for the API response.
    - Cleaned up App.tsx by consuming the new custom hook.

--- FrontendWeb/src/App.tsx
+++ FrontendWeb/src/App.tsx
@@ -1,40 +1,8 @@
-import { useEffect, useState } from 'react';
-
+import { useWeatherForecastHook } from './hooks/useWetherForecastHook';
 import './App.css';
 
-interface WeatherData {
-	date: string;
-	temperatureC: number;
-	temperatureF: number;
-	summary: string;
-}
-
 function App() {
-	const [forecasts, setForecasts] = useState<WeatherData[]>([]);
-	const [error, setError] = useState<string | null>(null);
-	const [loading, setLoading] = useState<boolean>(true);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch('api/weatherforecast');
-				if (!response.ok) {
-					throw new Error(`HTTP error! status: ${response.status}`);
-				}
-				const data = await response.json();
-				setForecasts(data);
-			} catch (err) {
-				setError(err instanceof Error ? err.message : 'Unknown error');
-			} finally {
-				setLoading(false);
-			}
-		};
-		fetchData();
-	}, []);
-
-	// fetch('http://localhost:5000/api/weatherforecast')
-	// 	.then((response) => response.json())
-	// 	.then((data) => setForecasts(data));
+	const { forecasts, error, loading } = useWeatherForecastHook();
 
 	return (
 		<div className='container'>
@@ -42,7 +10,7 @@ function App() {
 
 			<div className='card'>
 				{loading && <p>Ładowanie danych...</p>}
-				{error && <p style={{ color: 'red' }}>Błąd: {error}</p>}
+				{error && <p style={{ color: 'red' }}>Błąd: {error.message}</p>}
 				{!error && !loading && (
 					<p>
 						Poniżej znajduje się tabela z danymi pogodowymi pobranymi z
